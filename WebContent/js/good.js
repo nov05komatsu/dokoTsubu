b27bb9b0208a3ed485d0be695f519d0690b60041
KOMATSUZAKI Masato <nov05komatsu@gmail.com>
2022-11-21 12:28:26 +0900

    goodの数値を非同期で増減させることができるようにした

--- WebContent/js/good.js
+++ WebContent/js/good.js
@@ -1,13 +1,28 @@
 $(function(){
-	$('.goodButton').on('click', function(){
-		let g = $(this).next('span');
+	// goodを増やした時の処理関数
+	let setCancel = function(){
+		// イベントをいったん消去
+		$(this).off('click');
 		
+		// 要素の取得
+		let g = $(this).next('.goodSum');
+		
+		// ボタンの表示を変更		
+		$(this).text('cancel');
+		
+		// クラスの付け替え
+		$(this).toggleClass('goodButton cancelButton');
+		
+		// 新たにイベントをつけなおす
+		$(this).on('click', setGood);
+		
+		// 非同期通信
 		$.ajax({
 			url: "/dokoTsubu/Good",
 			type: "POST",
 			data: {
 				mutterId : $(this).val(),
-				good : g.text()
+				good : 1
 			}
 		}).done(function(result){
 			console.log(result);
@@ -16,5 +31,44 @@ $(function(){
 		}).fail(function(){
 			
 		});
-	});
+	}
+	
+	// cancelした時の処理関数
+	let setGood = function(){
+		// イベントをいったん消去
+		$(this).off('click');
+		
+		// 要素の取得
+		let g = $(this).next('.goodSum');
+		
+		// ボタンの表示を戻す		
+		$(this).text('good');
+		
+		// クラスの付け替え
+		$(this).toggleClass('cancelButton goodButton');
+		
+		// 新たにイベントをつけなおす
+		$(this).on('click', setCancel);
+		
+		// 非同期通信
+		$.ajax({
+			url: "/dokoTsubu/Good",
+			type: "POST",
+			data: {
+				mutterId : $(this).val(),
+				good : -1
+			}
+		}).done(function(result){
+			console.log(result);
+			g.text(result);
+			
+		}).fail(function(){
+			
+		});
+	}
+	
+	
+	$('.goodButton').on('click', setCancel);
+	
+	$('.cancelButton').on('click', setGood);
 });
